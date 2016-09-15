ac348d895fba6d981ecfb7d4f947a09d6ce2f62a
prutha96 <pruthadubhashi@gmail.com>
2016-09-15 15:50:53 +0530

    First Commit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	public static int gridWidth = 10;
+	public static int gridHeight = 20;
+
+	// Use this for initialization
+	void Start () {
+	
+		SpawnNextTetromino ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SpawnNextTetromino () {
+
+		GameObject nextTetromino = (GameObject)Instantiate (Resources.Load (GetRandomTetromino ()), new Vector2 (5.0f, 20.0f), Quaternion.identity);
+	}
+
+	public bool CheckIsInsideGrid (Vector2 pos) {
+	
+		return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+	}
+
+	public Vector2 Round (Vector2 pos) {
+	
+		return new Vector2 (Mathf.Round (pos.x), Mathf.Round (pos.y));
+	}
+
+	string GetRandomTetromino() {
+
+		int randomTetromino = Random.Range (1, 8);
+		string randomTetrominoName = "Prefabs/Tetromino_J";
+
+		switch (randomTetromino) {
+
+		case 1:
+			randomTetrominoName = "Prefabs/Tetromino_J";
+			break;
+		case 2:
+			randomTetrominoName = "Prefabs/Tetromino_L";
+			break;
+		case 3:
+			randomTetrominoName = "Prefabs/Tetromino_Long";
+			break;
+		case 4:
+			randomTetrominoName = "Prefabs/Tetromino_S";
+			break;
+		case 5:
+			randomTetrominoName = "Prefabs/Tetromino_Square";
+			break;
+		case 6:
+			randomTetrominoName = "Prefabs/Tetromino_T";
+			break;
+		case 7:
+			randomTetrominoName = "Prefabs/Tetromino_Z";
+			break;
+		}
+
+		return randomTetrominoName;
+	}
+}
---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tetromino : MonoBehaviour {
+
+	float fall = 0;
+	public float fallSpeed = 1;
+
+	public bool allowRotation = true;
+	public bool limitRotation = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		CheckUserInput ();
+	}
+
+	void CheckUserInput () {
+
+		if (Input.GetKeyDown (KeyCode.RightArrow) || Input.GetKeyDown (KeyCode.D)) {
+		
+			transform.position += new Vector3 (1, 0, 0);
+
+			if ((CheckIsValidPosition ())) {
+				
+			} else {
+				transform.position += new Vector3 (-1, 0, 0);
+			}
+
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.A)) {
+		
+			transform.position += new Vector3 (-1, 0, 0);
+
+			if ((CheckIsValidPosition ())) {
+				
+			} else {
+				transform.position += new Vector3 (1, 0, 0);
+			}
+
+		} else if (Input.GetKeyDown (KeyCode.DownArrow) || Input.GetKeyDown (KeyCode.S) || Time.time - fall >= fallSpeed) {
+
+			transform.position += new Vector3 (0, -1, 0);
+
+			if ((CheckIsValidPosition ())) {
+				
+			} else {
+				transform.position += new Vector3 (0, 1, 0);
+			}
+
+			fall = Time.time;
+
+		} else if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.W)) {
+		
+			if (allowRotation) {
+			
+				if (limitRotation) {
+		
+					if (transform.rotation.eulerAngles.z >= 90)
+						transform.Rotate (0, 0, -90);
+					else
+						transform.Rotate (0, 0, 90);
+
+				} else {
+
+					transform.Rotate (0, 0, 90); 
+				}
+
+				if ((CheckIsValidPosition ())) {
+					
+				} else {
+					
+					if (limitRotation) {
+
+						if (transform.rotation.eulerAngles.z >= 90)
+							transform.Rotate (0, 0, -90);
+						else
+							transform.Rotate (0, 0, 90);
+
+					} else {
+
+						transform.Rotate (0, 0, -90); 
+					}
+				}
+			}
+		}
+	} 
+
+	bool CheckIsValidPosition () {
+	
+		foreach (Transform mino in transform) {
+		
+			Vector2 pos = FindObjectOfType<Game> ().Round (mino.position);
+
+			if (FindObjectOfType<Game> ().CheckIsInsideGrid (pos) == false)
+				return false;
+		}
+
+		return true;
+	}
+}
